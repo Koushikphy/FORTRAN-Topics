adeb6ce71523dc44f0832760e3822426035a73fb
Koushik Naskar <koushik.naskar9@gmail.com>
2019-11-05 19:40:32 +0530

    up

--- factorial.f90
+++ factorial.f90
@@ -1,21 +1,34 @@
 program name
-    integer, external :: factorial1, factorial2, factorial3
-    print *, factorial1(5)
-    print *, factorial2(5)
-    print *, factorial3(5)
+    integer, external ::  factorial3
+    integer(kind=8), external :: factorial1
+    real(kind=8),external :: factorial2, fact
+    print *, factorial1(17)
+    print *, factorial2(17)
+    print *, fact(24)
+    print *, factorial3(-5)
 end program name
 
 
 function factorial1(x) result(y)
     implicit none
-    integer :: x,y,i
+    integer :: x,i
+    integer(kind=8)::y
     y = product((/(i,i=1,x)/))
 end function factorial1
 
 
+function fact(x) result(y)
+    implicit none
+    integer:: x,i
+    real(kind=8) :: y
+    y = product((/(i,i=1,x)/))
+end function fact
+
+
 function factorial2(x) result(y)
     implicit none
-    integer :: x,y,i
+    integer :: x,i
+    real(kind=8)::y
     if (x.eq.0) then
         y=1.0d0
     else
--- optional_keyword_argument.f90
+++ optional_keyword_argument.f90
@@ -13,11 +13,13 @@ program name
     ! keywords will be same as mentioned in interface block
     call asimplesub(xx=1,y=2,z=3)
     call asimplesub(z=3,xx=1,y=2)
+    call asimplesub(y=2,xx=1)
     ! prints----------------------------------
     ! 1           2         999
     ! 1           2           3
     ! 1           2           3
     ! 1           2           3
+    ! 1           2         999
     !-----------------------------------------
 end program name
 
