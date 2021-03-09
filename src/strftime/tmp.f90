f03d296183f99037846d5f7050d07984c734ccb4
Koushik Naskar <koushik.naskar9@gmail.com>
2021-03-10 00:55:33 +0530

    update 🚲

--- src/strftime/tmp.f90
+++ src/strftime/tmp.f90
@@ -1,24 +1,25 @@
-program name
+module timeStamp
     use iso_c_binding
-    implicit none
-
+    implicit none 
     interface
-        subroutine test(inp, out, n) bind(c,name= 'format_time')
-            use iso_c_binding
-            character(kind=c_char),intent(in) :: inp(*)
+        subroutine get(inp, out, n) bind(c,name= 'format_time')
+            import :: c_char, c_int, c_ptr
+            character(kind=c_char) :: inp(*), out(*)
             integer(kind=c_int),intent(out) :: n
-            type(c_ptr),intent(out) :: out
         end
     end interface
 
+    contains
+    function strftime(format) result(txt)
+        character(len=*) :: format
+        character(kind=c_char, len=100) :: txt
+        integer(c_int) :: n
+        call get(format//c_null_char, txt, n)
+        txt(n+1:) = ''
+    end
+end module
 
-    character(kind=c_char, len=50), pointer :: timestr
-    type(c_ptr) :: tt
-    integer(c_int) :: nn
-
-    call test("%B"//c_null_char, tt, nn)
-    call c_f_pointer(tt,timestr)
-    print *, timestr !<-- prints junk
-    print*, nn
-
+program name
+    use timeStamp
+    write(11,*) strftime('%x-%X')
 end program name
--- src/strftime/tt.c
+++ src/strftime/tt.c
@@ -5,16 +5,13 @@
 
 
 
-void format_time(char * argc, unsigned char ** output, int *n){
+void format_time(char * argc, char * output, int *n){
     time_t rawtime;
     struct tm * timeinfo;
-    char out[100];
     time ( &rawtime );
     timeinfo = localtime ( &rawtime );
-    strftime(out, 100, argc, timeinfo);
-    *output = out;
-    printf("%s\n",*output);
-    *n = strlen(*output); 
+    strftime(output, 100, argc, timeinfo);
+    *n = strlen(output); 
 }
 
 
